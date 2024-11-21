81226f8a4d38d7a1dcfbb33cb67a056a44aadd59
schrutebeet <ricardosans98@gmail.com>
2024-11-22 00:27:11 +0100

    add prints

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ last_hour = None
 
 while True:
     list_of_flights = flights.run_all()
+    print("All flights fetched.")
     df = DataManager.preprocess_data(list_of_flights)
+    print("Flights converted to dataframe.")
     DataManager.save_to_temp(df)
+    print("Flights dataframe saved in tmp folder.")
     last_hour = DataManager.process_hourly_data()
+    print(f"All files from {last_hour}h have been concatenated and stored.")
