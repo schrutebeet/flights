4f60042c6f9190d50eba570e8f2ca014a326b7d8
schrutebeet <ricardosans98@gmail.com>
2024-11-22 00:20:35 +0100

    add while true

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from flights_fetcher import FlightsFetcher
-
+from data_manager import DataManager
 
 flights = FlightsFetcher("https://www.flightaware.com/live/")
-flights.run_all()
+
+last_hour = None
+
+while True:
+    list_of_flights = flights.run_all()
+    df = DataManager.preprocess_data(list_of_flights)
+    DataManager.save_to_temp(df)
+    last_hour = DataManager.process_hourly_data()
