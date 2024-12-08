d6209ef4be81fba6cb7f23c3e8913339fb390fdc
schrutebeet <ricardosans98@gmail.com>
2024-12-08 13:01:21 +0100

    add number of flights fetched

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while True:
     list_of_flights = flights.run_all()
     print("All flights fetched.")
     df = DataManager.preprocess_data(list_of_flights)
-    print("Flights converted to dataframe.")
+    print(f"{len(df)} flights converted to dataframe.")
     DataManager.save_to_temp(df)
     last_hour = DataManager.process_hourly_data(last_hour=last_hour)
     print("\n\n")
