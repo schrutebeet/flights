3d5614b26abe4fd0dea9f735ebbe5adee6a96cd2
schrutebeet <ricardosans98@gmail.com>
2024-12-09 23:54:00 +0100

    add waiting time before storing data

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ while True:
     print("All flights fetched.")
     df = DataManager.preprocess_data(list_of_flights)
     print(f"{len(df)} flights converted to dataframe.")
-    DataManager.save_to_temp(df)
-    last_hour = DataManager.process_hourly_data(last_hour=last_hour)
     e = datetime.now()
     time_difference = (e - s).total_seconds()
     if time_difference < 60:
         sleep_time = 60 - time_difference
         time.sleep(sleep_time)
         print(f"Triggered time sleep for {sleep_time:.2f} seconds.")
+    DataManager.save_to_temp(df)
+    last_hour = DataManager.process_hourly_data(last_hour=last_hour)
     print("\n")
